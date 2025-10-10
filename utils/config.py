9abe990f351890e85a6dfa694888590e18c8e642
Limitless-One <harshavardhang513@gmail.com>
2025-10-10 16:42:11 +0530

    Create config.py
    Initial Settings values that the user can change

---
+++ utils/config.py
@@ -0,0 +1,15 @@
+# File format constants
+SUPPORTED_IMAGE_EXTS = {".jpg", ".jpeg", ".png", ".webp", ".bmp", ".avif"}
+SUPPORTED_DOC_EXTS = {".pdf"}
+HIDDEN_PREFIX = "."
+
+# UI Constants
+THUMB_BOX_SIZE = (170, 240)
+PREVIEW_MAX_SIZE = (150, 200)
+MIN_CELL_SPACING = 20
+MAX_CELL_SPACING = 40
+READER_SCROLL_STEP = 120
+
+# State file name
+STATE_FILE_NAME = "state.json"
+PREVIEW_CACHE_DIR = "previews"
