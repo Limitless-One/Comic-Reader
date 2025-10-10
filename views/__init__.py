2f81d476eabdfc618eca8968bd64b9559af2b048
Limitless-One <harshavardhang513@gmail.com>
2025-10-10 16:27:20 +0530

    Update __init__.py

--- views/__init__.py
+++ views/__init__.py
@@ -1,11 +1,1 @@
-"""
-views package
-~~~~~~~~~~~~~
-Convenience re‑exports so other modules can write:
-
-    from views import LibraryView, ApplicationWindow
-"""
-from .library_view import LibraryView  # noqa: F401
-from .chapter_view import ChapterListView  # noqa: F401
-from .reader_view import ReaderView  # noqa: F401
-from .application_window import ApplicationWindow  # noqa: F401
+#To initialize views as a package.
