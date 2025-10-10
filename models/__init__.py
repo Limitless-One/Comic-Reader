6936e4dfd8d4edff25d40da2bc25233a9daa16b2
Limitless-One <harshavardhang513@gmail.com>
2025-10-10 16:26:03 +0530

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,1 @@
-"""
-models package
-~~~~~~~~~~~~~~
-Exposes ComicLibrary, ComicState, ChapterState for easy import:
-    from models import ComicLibrary, ComicState
-"""
-from .comic_model import ComicLibrary, ComicState, ChapterState  # noqa: F401
+#To initialize the models package
